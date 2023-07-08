64c5d598f627b90872f9e80db0af3dee277a0933
Ashishwakde26 <Ashish261088@gmail.com>
2023-07-08 23:15:39 +0530

    Updated

--- app.js
+++ app.js
@@ -14,8 +14,31 @@ app.get('/', (req, res) => {
     res.sendFile(filename, options);
 })
 
+    // var users = 0;
+
+    var roomno = 1;
+    var full = 0;
+
 io.on('connection', (socket)=> {
     console.log("A user is connected.")
+
+    socket.join("room-"+roomno);
+
+    io.sockets.in("room-"+roomno).emit('connectedRoom', "You are connected to room no. "+roomno);
+
+    full++;
+
+    if(full >= 2) {
+        full = 0;
+        roomno++;
+    }
+
+    // users++;
+    /* io.sockets.emit('broadcast', {message: users + " users connected."}) */
+
+    // socket.emit('newuserconnect', { message: 'Hii, Welcome dear!'});
+
+    // socket.broadcast.emit('newuserconnect', {message: users + " users connected!"});
 
     // setTimeout(()=> {
     //     socket.send('Sent message from server side by prereserved event.');
@@ -25,13 +48,19 @@ io.on('connection', (socket)=> {
     //     socket.emit('myCustomEvent', {description: 'A custom message from server side!'})
     // }, 3000)
 
-    socket.on('myCustomClientEvent', (data)=> {
+    /* socket.on('myCustomClientEvent', (data)=> {
         console.log(data);
-    })
+    }) */
 
     socket.on('disconnect', ()=> {
         console.log("A user is disconnected.")
+
+        // users--;
+        // io.sockets.emit('broadcast', {message: users + " users connected."})
+        // socket.broadcast.emit('newuserconnect', {message: users + " users connected!"});
+        
     })
+
 });
 
 http.listen(3000, ()=> {
